00988aa9e33c608c1e67249c19f23a288b7863de
Sahil Chaudhary <sahil280114@gmail.com>
2024-09-26 17:55:45 +0200

    push latest process_dataset.py

--- process_dataset.py
+++ process_dataset.py
@@ -6,7 +6,7 @@ dataset = load_dataset("glaiveai/reflection-v1",split="train")
 def format_row(row):
     return f'''<|start_header_id|>system<|end_header_id|>{row["system"]}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{row["prompt"]}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n{row["response"]}<|eot_id|>'''
 
-processed_dataset = [format_row(row) for row in dataset]
+processed_dataset = [{"text":format_row(row)} for row in dataset]
 
 with open("reflection-data.json","w") as f:
     json.dump(processed_dataset,f)
